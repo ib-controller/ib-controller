a6363689e06be84c91525183f9a9c25999d4c95d
TheGreatGooo <thegreatgooo@gmail.com>
2018-06-13 23:59:57 -0400

    run under xvfb

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ apt-get install xvfb
 
 chmod u+x Scripts/IBController.sh
 
-Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
+/usr/sbin/xvfb-run --auto-servernum Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
