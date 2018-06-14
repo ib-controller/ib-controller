01389d848d4edb5aa6410ab39fc1a19b653ec775
TheGreatGooo <thegreatgooo@gmail.com>
2018-06-13 23:59:10 -0400

    temp install xvfb

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+apt-get install xvfb
+
 chmod u+x Scripts/IBController.sh
 
 Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
