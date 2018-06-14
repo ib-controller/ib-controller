80397f6f0b68d172b650505d91f436c0db92b795
TheGreatGooo <thegreatgooo@gmail.com>
2018-06-13 23:42:10 -0400

    add absolute dir for ibc path

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 chmod u+x Scripts/IBController.sh
 
-Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=target --ibc-ini=/ibc-config/IBController.ini
+Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
