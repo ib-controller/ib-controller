e2aed9c6ae0104efed6d5098299d84a64e902cc5
TheGreatGooo <thegreatgooo@gmail.com>
2018-06-20 23:23:20 -0400

    update install dir

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ apt-get -y install xvfb
 
 chmod u+x Scripts/IBController.sh
 
-/usr/sbin/xvfb-run --auto-servernum Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
+/usr/bin/xvfb-run --auto-servernum Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
