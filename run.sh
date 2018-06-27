4de97507f369fd3d263882a550a0d7ed6d64ede6
TheGreatGooo <thegreatgooo@gmail.com>
2018-06-26 22:45:40 -0400

    remove temporary workaround and tail logs

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-# this is a hopefully temporary hack!
-apt-get update
-apt-get -y install xvfb libxrender1 libxtst6
-
 chmod u+x Scripts/IBController.sh
+
+tail -F /tws/**/ibgateway.log &
 
 /usr/bin/xvfb-run --auto-servernum Scripts/IBController.sh 963 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini
