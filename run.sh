012412382c3cf0203ee8a78c5378942a3d84e768
TheGreatGooo <thegreatgooo@gmail.com>
2020-07-25 21:20:11 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ chmod u+x Scripts/IBController.sh
 
 tail -F /tws/**/ibgateway.log &
 
-/usr/bin/xvfb-run --auto-servernum Scripts/IBController.sh 972 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini --java-path=/opt/java/openjdk/bin/java 
+/usr/bin/xvfb-run --auto-servernum Scripts/IBController.sh 972 -g --tws-path=/tws --tws-settings-path=/tws --ibc-path=$PWD/target --ibc-ini=/ibc-config/IBController.ini --java-path=/opt/java/openjdk/bin 
